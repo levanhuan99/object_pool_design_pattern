4833eec5bb59d4d0f1dca633c3f3b9944c1e3494
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-05-23 09:00:52 +0700

    click me

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        // Create the ConnectionPool:
+        JDBCConnectionPool pool = new JDBCConnectionPool(
+                "org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+                "sa", "secret");
+
+        // Get a connection:
+        Connection con = pool.checkOut();
+
+        // Use the connection
+
+
+        // Return the connection:
+        pool.checkIn(con);
+
+    }
+
+}
